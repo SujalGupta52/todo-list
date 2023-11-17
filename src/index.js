5e63b4ab565ac80d69a865b4ea1f6d6407d39f07
SujalGupta52 <67575635+SujalGupta52@users.noreply.github.com>
2023-11-17 23:30:39 +0530

    Complete UI elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-document.querySelector('div').textContent = 'Test'
+class todo {
+    constructor(project, title, description, priority, isChecked) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.isChecked = false;
+    }
+
+}
+
+const temp = new todo('test', 'test', 'high', true)
+
+todo.prototype.toggleChecked = () => {
+        this.isChecked = this.isChecked == true ? false : true;
+}
+
+    
