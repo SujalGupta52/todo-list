88e332319c56dee6c7a5b25e49885614f51b0fd6
SujalGupta52 <67575635+SujalGupta52@users.noreply.github.com>
2023-11-22 12:13:53 +0530

    Create DOMController  class to handle DOM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-class todo {
-    constructor(project, title, description, priority, isChecked) {
-        this.title = title;
-        this.description = description;
-        this.priority = priority;
-        this.isChecked = false;
+class DOMController {
+  static toggleProjectListInput() {
+    const projectInput = document.createElement("input");
+
+    projectInput.type = "text";
+    projectInput.classList.toggle("new-project");
+
+    if (document.querySelector(".new-project") != null) {
+      document
+        .querySelector(".project-list")
+        .removeChild(document.querySelector(".new-project"));
+    } else {
+      document.querySelector(".project-list").appendChild(projectInput);
     }
+  }
 
-}
+  static toggleNewNoteDialog() {
+    const dialog = document.querySelector(".new-note");
+    console.log(dialog.style.visibility);
+    dialog.style.visibility =
+      dialog.style.visibility != "visible" ? "visible" : "hidden";
+  }
 
-const temp = new todo('test', 'test', 'high', true)
+  static appendNote(title, priority, isChecked) {
+    const content = document.querySelector(".content");
+    const todo = document.createElement("div");
+    const checkbox = document.createElement("input");
+    const text = document.createElement("span");
 
-todo.prototype.toggleChecked = () => {
-        this.isChecked = this.isChecked == true ? false : true;
-}
+    text.textContent = title;
 
+    checkbox.type = "checkbox";
+
+    todo.classList.toggle("item");
+    todo.style.borderLeftColor = "yellow";
+
+    todo.appendChild(checkbox);
+    todo.appendChild(text);
+
+    content.appendChild(todo);
+  }
+
+  static appendProject(name, color, isActive) {
+    const project = document.createElement('li');
+    const text = document.createElement('span');
+    const colorBox = document.createElement('div');
+    const projectList = document.querySelector('.project-list');
+
+    if(isActive) project.classList.toggle('active');
+    colorBox.classList.toggle('color');
+    text.classList.toggle('text');
+    text.textContent = name;
+    colorBox.style.backgroundColor = color;
     
+    project.appendChild(colorBox);
+    project.appendChild(text);
+    projectList.appendChild(project);
+  }
+}
+
+
